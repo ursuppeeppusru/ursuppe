5a30a46f47ad257ff190bab6af2850b428e723f7
anders aardvark <aarvik92@gmail.com>
2024-02-28 10:03:14 +0000

    Archvive year filter

--- ps_submission/views.py
+++ ps_submission/views.py
@@ -7,6 +7,14 @@ from django.views.decorators.cache import cache_page
 from .forms import ExhibitionSubmissionForm, ExhibitionImagesForm
 from .models import ExhibitionImages, ExhibitionSubmission
 
+def unique(list):
+    # remove duplicates from list
+    result = []
+    for i in list:
+        if i not in result:
+            result.append(i)
+
+    return result
 
 def exhibition_submission_create(request):
     # ImageFormSet = modelformset_factory(ExhibitionImages, form=ExhibitionImagesForm, extra=0)
@@ -60,7 +68,23 @@ def exhibition_submission_create(request):
 def exhibition_list(request):
     # Only object which are marked as published
     submissions = ExhibitionSubmission.objects.filter(published=True).order_by('-exhibition_end')
-    return render(request, 'submission_list.html', {'submissions': submissions})
+
+    # Get unique years and pass to template
+    years = ExhibitionSubmission.objects.filter(published=True).values_list('exhibition_opening__year', flat=True)
+    unique_years = unique(years)
+
+    return render(request, 'submission_list.html', {'submissions': submissions, 'years': unique_years})
+
+@cache_page(60 * 60)
+def exhibition_list_year(request, year):
+    # Only object which are marked as published sorted by year
+    submissions = ExhibitionSubmission.objects.filter(published=True, exhibition_opening__year=year).order_by('-exhibition_end')
+
+    # Get unique years and pass to template
+    years = ExhibitionSubmission.objects.filter(published=True).values_list('exhibition_opening__year', flat=True)
+    unique_years = unique(years)
+
+    return render(request, 'submission_list.html', {'submissions': submissions, 'years': unique_years})
 
 @cache_page(60 * 60)
 def exhibition_submission_detail(request, submission_id, submission_project_title):
