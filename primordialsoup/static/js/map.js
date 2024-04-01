181c0f824b459bcbd54d4ec9e96acb2eb349b2df
Sofie Meta Schroeder <sofiemeta@gmail.com>
2024-04-01 12:29:09 +0200

    Working on markers and clusters

--- primordialsoup/static/js/map.js
+++ primordialsoup/static/js/map.js
@@ -1,30 +1,22 @@
-// Initialize Leaflet map
-var map = L.map('map').setView([56.26904388487482, 10.755436652621228], 7);
+let map = L.map('map').setView([56.26904388487482, 10.755436652621228], 7);
 
-// Add base map layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Create a marker cluster group
-var markers = L.markerClusterGroup({
-    spiderfyOnMaxZoom: false,
-    showCoverOnHover: false,
-    zoomToBoundsOnClick: false
+
+// Improvement: adjust spiderify to exact coordinates
+// Improvement: Hide cluster icon when clicked
+let markers = L.markerClusterGroup({
+    iconCreateFunction: function (cluster) {
+        var markers = cluster.getAllChildMarkers();
+        var html = '<div class="circle">' + markers.length + '</div>';
+        return L.divIcon({ html: html, className: 'mycluster', iconSize: L.point(0, 40) });
+    },
+    spiderfyOnMaxZoom: true, showCoverageOnHover: false, zoomToBoundsOnClick: true,
+    maxClusterRadius: 60,
+    
 });
-
-
-// // Click or hover to wake map - see: http://cliffcloud.github.io/Leaflet.Sleep/#summary 
-// map.on('click', function () {
-//     if (map.scrollWheelZoom.enabled()) {
-//         map.scrollWheelZoom.disable();
-//     }
-//     else {
-//         map.scrollWheelZoom.enable();
-//     }
-// });
-
-
 
 const jsonUrl = 'http://127.0.0.1:8000/events/json';
 
@@ -34,15 +26,14 @@ fetch(jsonUrl)
     .then(data => {
         // Loop through the data
         data.event_submissions_json.forEach(event => {
-            // Extract necessary information
-            var id = event.id;
-            var latitude = parseFloat(event.latitude);
-            var longitude = parseFloat(event.longitude);
+            let id = event.id;
+            let latitude = parseFloat(event.latitude);
+            let longitude = parseFloat(event.longitude);
 
-            // Create marker
-            console.log('latitude: ' + latitude + 'longitude: ' + longitude)
-            var marker = L.marker([latitude, longitude], {icon: soupIcon}).addTo(map);
-            marker.bindPopup(`Event ID: ${id}`);
+            // console.log('latitude: ' + latitude + 'longitude: ' + longitude)
+            let marker = L.marker([latitude, longitude], {icon: soupIcon});
+            marker.bindPopup(`<img style="border-radius: 50%;" src="https://kunsten.nu/wp-content/uploads/2024/03/gaza-18-904x603.jpg">`);
+            // event image url will be part of json `<img style="border-radius: 50%;" src=${event.image-url}>`
 
             // Add marker to the marker cluster group
             markers.addLayer(marker);
@@ -61,46 +52,54 @@ fetch(jsonUrl)
         console.error('Error fetching data:', error);
     });
 
-// Function to show event details
+
+// Improvement: Click or hover to wake map - see: http://cliffcloud.github.io/Leaflet.Sleep/#summary 
+// map.on('click', function () {
+//     if (map.scrollWheelZoom.enabled()) {
+//         map.scrollWheelZoom.disable();
+//     }
+//     else {
+//         map.scrollWheelZoom.enable();
+//     }
+// });
+
+// Improvement: get EventDetails based on cards. Kan man bruge django templating i js?
 function showEventDetails(event) {
-    // Hide all event details divs
-    var eventDetailsDiv = document.getElementById('eventDetails');
+    const eventDetailsDiv = document.getElementById('eventDetails');
     eventDetailsDiv.innerHTML = `
-    <div class="row content">
-    <div class="col-4">
-    <br/>
-    <br/>
-        <h4>${event.location }</h4>
-        <p>${event.location_address}</p>
-        <br/>
-        <p><small><a href="${event.link_to_location}" target="_blank"> ${event.link_to_location }</a></small></p>
-    </div>
-    <div class="col-4 text-center">
-    <br/>
-    <br/>
-        <p>${event.artists}</p>
-        <a class="event_project_title_link" href="{% url 'event_detail' event_id=submission.id event_project_title=submission.slug %}">
-            <h2 class="text-uppercase is-large event_project_title">${event.project_title }</h2>
-            <img class="event-submission-image-list" src="{{ submission.images.all.0.image.url }}" alt="{{ submission.images.all.0.image.caption }}">
-        </a>
-        <br/>
-        <p>Curated by ${event.curators}</p>
-        <br/>
-        <p><small>${event.event_type }</small></p>
-    </div>
-    <div class="col-4 text-right">
-    <br/>
-    <br/>
-        <h4>${event.exhibition_opening} – ${event.exhibition_end}</h4>
-        <br/>
-        <p>Opening hours:</p>
-        <p>${event.opening_hours}</p>
-        <br/>
-        <p><small>Admission:</small></p>
-        <p><small>${event.admission }</small></p>
+        <div class="row content events-list-card">
+            <div class="col-4 ">
+                <p>${event.artists}</p>
+                <a class="event-project-title-link" href="/events/${event.id}-${event.slug}">
+                    <h2 class="text-uppercase is-large event_project_title">${event.project_title}</h2>
+                </a>
+                <img class="event-submission-image-list" src="{event.images.all.0.image.url}" alt="{event.images.all.0.image.caption}">
+                <br/>
+                <p>Curated by ${event.curators}</p>
+                <br/>
+                <p><small>${event.event_type}</small></p>
+            </div>
+            <div class="col-4">
+                <h4>${event.exhibition_opening} → ${event.exhibition_end}</h4>
+                <br/>
+                <p>Weekly opening hours:</p>
+                <p>${event.opening_hours}</p>
+                <br/>
+                <h4>Opening/vernissage: ${event.opening}</h4>
+                <br/>
+                <p>Opening hours for opening/vernissage:</p>
+                <p>${event.opening_hours_for_opening_date}</p>
+            </div>
+            <div class="col-4">
+                <h4>${event.location}</h4>
+                <p>${event.location_address}</p>
+                <br/>
+                <p><small><a href="${event.link_to_location}" target="_blank">${event.link_to_location}</a></small></p>
+                <br/>
+                <p><small>Admission:</small></p>
+                <p><small>${event.admission}</small></p>
+            </div>
         </div>
-        </div>
-        <hr/>
     `;
     eventDetailsDiv.style.display = 'block';
 }
@@ -111,11 +110,19 @@ map.on('popupclose', function() {
     eventDetailsDiv.style.display = 'none';
 });
 
-
 // marker icons
-const soupIcon = L.icon({
-    iconUrl: '../media/images/icons/circle.png',
-    iconSize:     [, 40], // size of the icon
-    iconAnchor:   [20, 8], // point of the icon which will correspond to marker's location
+const soupIcon = L.divIcon({
+//    iconUrl: '/static/image/icons/circle.png',
+    className: 'soup',
+    html: '<div class="circle"></div>',
+    iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
     popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
 });
+
+
+// html id: event-detail-map 
+const eventDetailMap  = L.map('eventDetailMap').setView([56.26904388487482, 10.755436652621228], 12);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(eventDetailMap);
