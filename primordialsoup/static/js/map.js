2a617255b9e0008ce1058bf8ad1a984a446f1ab1
Anders Aarvik <aarvik92@gmail.com>
2024-05-23 22:09:10 +0200

    Release/v1.0.10 (#29)
    * Tiny frontend changes and added timezone and language to .env file (#27)
    
    * Add h5 instead of p in right buttons, and add timezone and language to .env file
    
    * Added scrollbar styling to mobile nav.tabs
    
    * Add group exhibition text instead of list of artists in highlights
    
    * Fixed bottom padding/margin
    
    * Refactoring and styling of layer controls (#26)
    
    * Add parent and sub layer for all and current events respectively
    
    * Add leaflet .js and .css files to commit
    
    * Change label closing soon to only be on events that also are current
    
    * Add zoom when marker is clicked, on event details map
    
    * Add zoom out when marker is clicked when zoomed in
    
    * Add toggle button to map
    
    * Fixed exhibitions label 'closing soon' to only be on current exhibitions in event details and highlights
    
    * Add leaflet.css and .js
    
    * Add custom checkboxes
    
    * Styling of layer control incl. checkboxes
    
    * Adjusted map sleep time to 250ms
    
    * Title in django-admin refers to .env-file
    
    * Event detail map height same as main map
    
    * Tidy .css of empty declaration blocks and invalid syntax as pointed out by VSCode
    
    * BasteleurBold now secondary header font
    
    ---------
    
    Co-authored-by: Sofie Meta <43446790+atmeta@users.noreply.github.com>

--- primordialsoup/static/js/map.js
+++ primordialsoup/static/js/map.js
@@ -32,10 +32,18 @@ const clusterSettings = {
     maxClusterRadius: 30,
 }
 
-// Cluster groups
+// Layer groups
 const allEvents = L.markerClusterGroup(clusterSettings);
+const upcomingEvents = L.featureGroup.subGroup(allEvents);
 const currentEvents = L.featureGroup.subGroup(allEvents);
+allEvents.addTo(map);
 
+// Layer control 
+const control = L.control.layers(null, null, { collapsed: false});
+control.addOverlay(upcomingEvents, "Upcoming events");
+control.addOverlay(currentEvents, "Current events");
+control.addTo(map);
+  
 // Fetch JSON
 const jsonUrl = '/events/json';
 fetch(jsonUrl).then(response => response.json()).then(data => {
@@ -48,19 +56,21 @@ fetch(jsonUrl).then(response => response.json()).then(data => {
             const marker = L.marker([latitude, longitude], {icon: soupIcon});
             
             marker.bindPopup(`<img style="border-radius: 50%;" src="/media/${image}">`);
-            
-            if (new Date(eventOpening) >= new Date()) {
-                currentEvents.addLayer(marker);
+
+            if (new Date(eventOpening) > new Date()) {
+                upcomingEvents.addLayer(marker);
+                console.log('Marker added to upcoming events!')
             } else {
-                allEvents.addLayer(marker);
+                currentEvents.addLayer(marker);
+                console.log('Marker added to current events!')
             };
             
             marker.on('click', function() {
                 showEventDetails(event);
             });
 
-            map.addLayer(currentEvents);
-            map.addLayer(allEvents);
+            upcomingEvents.addTo(map);
+            currentEvents.addTo(map);
         });
     })
     .catch(error => {
@@ -68,7 +78,7 @@ fetch(jsonUrl).then(response => response.json()).then(data => {
 });
 
 // Formating JSON date for event details
-const formatJsonDate = (jsonDate) => {
+function formatJsonDate(jsonDate) {
     const date = new Date(jsonDate);
     const options = {
         year: 'numeric',
