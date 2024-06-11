e117ca09844f544d2aaeb9d43d58d2823157a34f
Anders Aarvik <aarvik92@gmail.com>
2024-06-11 22:34:43 +0200

    Squashed commit of the following:
    commit 8697c636ade997b08f2b958bb99452af77b86a60
    Author: Anders Aarvik <aarvik92@gmail.com>
    Date:   Tue Jun 11 22:33:47 2024 +0200
    
        One-day event option

--- primordialsoup/static/js/primordialsoup.js
+++ primordialsoup/static/js/primordialsoup.js
@@ -44,185 +44,221 @@ if(setDarkMode === 'on') {
 }
 
 /* ICS file generator */
-/**
-* Create and download a file on click
-* @params {string} filename - The name of the file with the ending
-* @params {string} filebody - The contents of the file
-*/
-function download(filename, fileBody) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(fileBody));
-    element.setAttribute('download', filename);
+// Only for Event Detail page
+if(document.body.classList.contains('event-detail')) {
+    /**
+    * Create and download a file on click
+    * @params {string} filename - The name of the file with the ending
+    * @params {string} filebody - The contents of the file
+    */
+    function download(filename, fileBody) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(fileBody));
+        element.setAttribute('download', filename);
 
-    element.style.display = 'none';
-    document.body.appendChild(element);
+        element.style.display = 'none';
+        document.body.appendChild(element);
 
-    element.click();
+        element.click();
 
-    document.body.removeChild(element);
-}
+        document.body.removeChild(element);
+    }
 
 
-/**
-* Returns a date/time in ICS format
-* @params {Object} dateTime - A date object you want to get the ICS format for.
-* @returns {string} String with the date in ICS format
-*/
-function convertToICSDate(dateTime) {
-    const year = dateTime.getFullYear().toString();
-    const month = (dateTime.getMonth() + 1) < 10 ? "0" + (dateTime.getMonth() + 1).toString() : (dateTime.getMonth() + 1).toString();
-    const day = dateTime.getDate() < 10 ? "0" + dateTime.getDate().toString() : dateTime.getDate().toString();
-    const hours = dateTime.getHours() < 10 ? "0" + dateTime.getHours().toString() : dateTime.getHours().toString();
-    const minutes = dateTime.getMinutes() < 10 ? "0" +dateTime.getMinutes().toString() : dateTime.getMinutes().toString();
+    /**
+    * Returns a date/time in ICS format
+    * @params {Object} dateTime - A date object you want to get the ICS format for.
+    * @returns {string} String with the date in ICS format
+    */
+    function convertToICSDate(dateTime) {
+        const year = dateTime.getFullYear().toString();
+        const month = (dateTime.getMonth() + 1) < 10 ? "0" + (dateTime.getMonth() + 1).toString() : (dateTime.getMonth() + 1).toString();
+        const day = dateTime.getDate() < 10 ? "0" + dateTime.getDate().toString() : dateTime.getDate().toString();
+        const hours = dateTime.getHours() < 10 ? "0" + dateTime.getHours().toString() : dateTime.getHours().toString();
+        const minutes = dateTime.getMinutes() < 10 ? "0" +dateTime.getMinutes().toString() : dateTime.getMinutes().toString();
 
-    return year + month + day + "T" + hours + minutes + "00";
-}
+        return year + month + day + "T" + hours + minutes + "00";
+    }
 
-/**
-* Creates and downloads an ICS file
-* @params {string} timeZone - In the format America/New_York
-* @params {object} startTime - Vaild JS Date object in the event timezone
-* @params {object} endTime - Vaild JS Date object in the event timezone
-* @params {string} title
-* @params {string} description
-* @params {string} venueName
-* @params {string} address
-*/
-function createDownloadICSFile(timezone, startTime, endTime, title, description, venueName, address) {
-  const icsBody = 'BEGIN:VCALENDAR\n' +
-  'VERSION:2.0\n' +
-  'PRODID:Calendar\n' +
-  'CALSCALE:GREGORIAN\n' +
-  'METHOD:PUBLISH\n' +
-  'BEGIN:VTIMEZONE\n' +
-  'TZID:' + timezone + '\n' +
-  'END:VTIMEZONE\n' +
-  'BEGIN:VEVENT\n' +
-  'SUMMARY:' + title + '\n' +
-  'UID:@Default\n' +
-  'SEQUENCE:0\n' +
-  'STATUS:CONFIRMED\n' +
-  'TRANSP:TRANSPARENT\n' +
-  'DTSTART;TZID=' + timezone + ':' + convertToICSDate(startTime) + '\n' +
-  'DTEND;TZID=' + timezone + ':' + convertToICSDate(endTime)+ '\n' +
-  'DTSTAMP:'+ convertToICSDate(new Date()) + '\n' +
-  'LOCATION:' + venueName + ' (' + address + ')\n' +
-  'DESCRIPTION:' + description + '\n' +
-  'END:VEVENT\n' +
-  'END:VCALENDAR\n';
+    /**
+    * Creates and downloads an ICS file
+    * @params {string} timeZone - In the format America/New_York
+    * @params {object} startTime - Vaild JS Date object in the event timezone
+    * @params {object} endTime - Vaild JS Date object in the event timezone
+    * @params {string} title
+    * @params {string} description
+    * @params {string} venueName
+    * @params {string} address
+    */
+    function createDownloadICSFile(timezone, startTime, endTime, title, description, venueName, address) {
+      const icsBody = 'BEGIN:VCALENDAR\n' +
+      'VERSION:2.0\n' +
+      'PRODID:Calendar\n' +
+      'CALSCALE:GREGORIAN\n' +
+      'METHOD:PUBLISH\n' +
+      'BEGIN:VTIMEZONE\n' +
+      'TZID:' + timezone + '\n' +
+      'END:VTIMEZONE\n' +
+      'BEGIN:VEVENT\n' +
+      'SUMMARY:' + title + '\n' +
+      'UID:@Default\n' +
+      'SEQUENCE:0\n' +
+      'STATUS:CONFIRMED\n' +
+      'TRANSP:TRANSPARENT\n' +
+      'DTSTART;TZID=' + timezone + ':' + convertToICSDate(startTime) + '\n' +
+      'DTEND;TZID=' + timezone + ':' + convertToICSDate(endTime)+ '\n' +
+      'DTSTAMP:'+ convertToICSDate(new Date()) + '\n' +
+      'LOCATION:' + venueName + ' (' + address + ')\n' +
+      'DESCRIPTION:' + description + '\n' +
+      'END:VEVENT\n' +
+      'END:VCALENDAR\n';
 
-  download(title + '.ics', icsBody);
+      download(title + '.ics', icsBody);
+    }
 }
 
 /* index horisontal sliders */
+// Only for Highlights/index page
+if(document.body.classList.contains('highlights')) {
+    // Cache DOM elements
+    var eventsRow = document.getElementById('events-index-row');
+    var buttonOne = document.getElementById('slideOne');
+    var backOne = document.getElementById('slideBackOne');
+    var buttonTwo = document.getElementById('slideTwo');
+    var backTwo = document.getElementById('slideBackTwo');
+    var buttonThree = document.getElementById('slideThree');
+    var backThree = document.getElementById('slideBackThree');
 
-// Cache DOM elements
-var eventsRow = document.getElementById('events-index-row');
-var buttonOne = document.getElementById('slideOne');
-var backOne = document.getElementById('slideBackOne');
-var buttonTwo = document.getElementById('slideTwo');
-var backTwo = document.getElementById('slideBackTwo');
-var buttonThree = document.getElementById('slideThree');
-var backThree = document.getElementById('slideBackThree');
+    // Calculate width once
+    var width = eventsRow.offsetWidth - 50;
 
-// Calculate width once
-var width = eventsRow.offsetWidth - 50;
+    // Consolidated event handler function
+    function setupSliderEvents(button, back, containerId) {
+        button.onclick = function () {
+            var container = document.getElementById(containerId);
+            smoothScroll(container, 'right', width);
+        };
 
-// Consolidated event handler function
-function setupSliderEvents(button, back, containerId) {
-    button.onclick = function () {
-        var container = document.getElementById(containerId);
-        smoothScroll(container, 'right', width);
-    };
-
-    back.onclick = function () {
-        var container = document.getElementById(containerId);
-        smoothScroll(container, 'left', width);
-    };
-}
-
-// Setup event listeners
-setupSliderEvents(buttonOne, backOne, 'scroll-containerOne');
-setupSliderEvents(buttonTwo, backTwo, 'scroll-containerTwo');
-setupSliderEvents(buttonThree, backThree, 'scroll-containerThree');
-
-// Easing function
-function easeInOutQuad(t) {
-    return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-}
-
-// Smooth scroll function
-function smoothScroll(element, direction, distance) {
-    var start = element.scrollLeft;
-    var startTime = performance.now();
-
-    function scroll(time) {
-        var elapsed = time - startTime;
-        var progress = Math.min(elapsed / 500, 1); // Duration: 500ms
-
-        if (direction === 'left') {
-            element.scrollLeft = start - (distance * easeInOutQuad(progress));
-        } else {
-            element.scrollLeft = start + (distance * easeInOutQuad(progress));
-        }
-
-        if (progress < 1) {
-            requestAnimationFrame(scroll);
-        }
+        back.onclick = function () {
+            var container = document.getElementById(containerId);
+            smoothScroll(container, 'left', width);
+        };
     }
 
-    requestAnimationFrame(scroll);
+    // Setup event listeners
+    setupSliderEvents(buttonOne, backOne, 'scroll-containerOne');
+    setupSliderEvents(buttonTwo, backTwo, 'scroll-containerTwo');
+    setupSliderEvents(buttonThree, backThree, 'scroll-containerThree');
+
+    // Easing function
+    function easeInOutQuad(t) {
+        return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+    }
+
+    // Smooth scroll function
+    function smoothScroll(element, direction, distance) {
+        var start = element.scrollLeft;
+        var startTime = performance.now();
+
+        function scroll(time) {
+            var elapsed = time - startTime;
+            var progress = Math.min(elapsed / 500, 1); // Duration: 500ms
+
+            if (direction === 'left') {
+                element.scrollLeft = start - (distance * easeInOutQuad(progress));
+            } else {
+                element.scrollLeft = start + (distance * easeInOutQuad(progress));
+            }
+
+            if (progress < 1) {
+                requestAnimationFrame(scroll);
+            }
+        }
+
+        requestAnimationFrame(scroll);
+    }
 }
 
 /* index archive selector */
-document.addEventListener("DOMContentLoaded", function() {
-    const tabLinks = document.querySelectorAll(".tab-link");
-    const imageContainers = document.querySelectorAll(".image-container-list");
-    let currentIndex = 0;
-    let timer; 
+// Only for Highlights/index page
+if(document.body.classList.contains('highlights')) {
+    document.addEventListener("DOMContentLoaded", function() {
+        const tabLinks = document.querySelectorAll(".tab-link");
+        const imageContainers = document.querySelectorAll(".image-container-list");
+        let currentIndex = 0;
+        let timer; 
 
-    // Function to show image container based on index
-    function showImageByIndex(index) {
-        tabLinks.forEach(function(link) {
-            link.classList.remove("active");
-        });
-        tabLinks[index].classList.add("active");
+        // Function to show image container based on index
+        function showImageByIndex(index) {
+            tabLinks.forEach(function(link) {
+                link.classList.remove("active");
+            });
+            tabLinks[index].classList.add("active");
 
-        imageContainers.forEach(function(container) {
-            container.classList.remove("active");
-        });
-        imageContainers[index].classList.add("active");
-    }
+            imageContainers.forEach(function(container) {
+                container.classList.remove("active");
+            });
+            imageContainers[index].classList.add("active");
+        }
 
-    // Function to switch to the next tab and image container
-    function switchToNextImage() {
-        currentIndex = (currentIndex + 1) % tabLinks.length; // Increment index and wrap around
-        showImageByIndex(currentIndex);
-    }
-
-    // Add mouseover event listener to tab links
-    tabLinks.forEach(function(link, index) {
-        link.addEventListener("mouseover", function(e) {
-            e.preventDefault();
-            currentIndex = index; // Update current index when mouseover occurs
+        // Function to switch to the next tab and image container
+        function switchToNextImage() {
+            currentIndex = (currentIndex + 1) % tabLinks.length; // Increment index and wrap around
             showImageByIndex(currentIndex);
+        }
+
+        // Add mouseover event listener to tab links
+        tabLinks.forEach(function(link, index) {
+            link.addEventListener("mouseover", function(e) {
+                e.preventDefault();
+                currentIndex = index; // Update current index when mouseover occurs
+                showImageByIndex(currentIndex);
+            });
+        });
+
+        // Start the timer to switch images automatically every 5 seconds
+        timer = setInterval(switchToNextImage, 5000);
+
+        // Stop the timer when mouseover occurs on tab links
+        tabLinks.forEach(function(link) {
+            link.addEventListener("mouseover", function() {
+                clearInterval(timer);
+            });
+        });
+
+        // Restart the timer when mouse leaves the tab links
+        tabLinks.forEach(function(link) {
+            link.addEventListener("mouseleave", function() {
+                timer = setInterval(switchToNextImage, 5000);
+            });
         });
     });
+}
 
-    // Start the timer to switch images automatically every 5 seconds
-    timer = setInterval(switchToNextImage, 5000);
+/* event form one-day event toggle */
+// Only for Create Event page
+if(document.body.classList.contains('event-form')) {
+    document.addEventListener('DOMContentLoaded', function () {
+        const oneDayEventCheckbox = document.querySelector('#id_one_day_event');
+        const exhibitionOpeningField = document.querySelector('#id_exhibition_opening').closest('p');
+        const exhibitionEndField = document.querySelector('#id_exhibition_end').closest('p');
+        const openingHoursField = document.querySelector('#id_opening_hours').closest('p');
 
-    // Stop the timer when mouseover occurs on tab links
-    tabLinks.forEach(function(link) {
-        link.addEventListener("mouseover", function() {
-            clearInterval(timer);
-        });
-    });
+        function toggleFields() {
+          if (oneDayEventCheckbox.checked) {
+            exhibitionOpeningField.style.display = 'none';
+            exhibitionEndField.style.display = 'none';
+            openingHoursField.style.display = 'none';
+          } else {
+            exhibitionOpeningField.style.display = 'block';
+            exhibitionEndField.style.display = 'block';
+            openingHoursField.style.display = 'block';
+          }
+        }
 
-    // Restart the timer when mouse leaves the tab links
-    tabLinks.forEach(function(link) {
-        link.addEventListener("mouseleave", function() {
-            timer = setInterval(switchToNextImage, 5000);
-        });
-    });
-});
+        oneDayEventCheckbox.addEventListener('change', toggleFields);
+
+        // Initial call to set the correct state on page load
+        toggleFields();
+    });    
+}
--- primordialsoup/views.py
+++ primordialsoup/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.views.decorators.cache import cache_page
 from datetime import timedelta
+from django.db.models import Q
 
 from ps_submission.models import ExhibitionSubmission
 from ps_calendar.models import CalendarSubmission
@@ -17,7 +18,7 @@ def today_plus_2w():
 def highlights(request):
     # Only object which are marked as published
     highlighted_submissions = ExhibitionSubmission.objects.filter(published=True, highlight=True).order_by('-exhibition_end')[:5]
-    highlighted_event_submissions = CalendarSubmission.objects.filter(published=True, highlight=True).filter(exhibition_end__gte=today()).order_by('exhibition_end')
-    all_current_event_submissions = CalendarSubmission.objects.filter(published=True).filter(exhibition_end__gte=today()).filter(exhibition_opening__lte=today()).order_by('exhibition_end')
-    all_upcoming_event_submissions = CalendarSubmission.objects.filter(published=True).filter(exhibition_opening__gte=today()).order_by('exhibition_opening')
+    highlighted_event_submissions = CalendarSubmission.objects.filter(published=True, highlight=True).filter(Q(exhibition_end__gte=today()) | Q(opening__gte=today())).order_by('exhibition_end')
+    all_current_event_submissions = CalendarSubmission.objects.filter(published=True).filter(Q(exhibition_end__gte=today()) | Q(opening__contains=today())).filter(Q(exhibition_opening__lte=today()) | Q(opening__contains=today())).order_by('exhibition_end')
+    all_upcoming_event_submissions = CalendarSubmission.objects.filter(published=True).filter(Q(exhibition_opening__gte=today()) | Q(opening__gt=today())).order_by('exhibition_opening')
     return render(request, 'highlights.html', {'highlighted_submissions': highlighted_submissions, 'highlighted_event_submissions': highlighted_event_submissions, 'all_current_event_submissions': all_current_event_submissions, 'all_upcoming_event_submissions': all_upcoming_event_submissions, 'today': today(), 'today_plus_2w': today_plus_2w()})
