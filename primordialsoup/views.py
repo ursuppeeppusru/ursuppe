c6d358ee09a7f0dbb493760f3ac6a851f719cc78
Anders Aarvik <aarvik92@gmail.com>
2024-08-18 17:24:10 +0200

    Sort upcoming events based on opening/vernissage date instead of general activity period (#37)

--- primordialsoup/views.py
+++ primordialsoup/views.py
@@ -20,5 +20,5 @@ def highlights(request):
     highlighted_submissions = ExhibitionSubmission.objects.filter(published=True, highlight=True).order_by('-created_at')[:5]
     highlighted_event_submissions = CalendarSubmission.objects.filter(published=True, highlight=True).filter(Q(exhibition_end__gte=today()) | Q(opening__gte=today())).order_by('exhibition_end')
     all_current_event_submissions = CalendarSubmission.objects.filter(published=True).filter(Q(exhibition_end__gte=today()) | Q(opening__contains=today())).filter(Q(exhibition_opening__lte=today()) | Q(opening__contains=today())).order_by('exhibition_end')
-    all_upcoming_event_submissions = CalendarSubmission.objects.filter(published=True).filter(Q(exhibition_opening__gte=today()) | Q(opening__gt=today())).order_by('exhibition_opening')
+    all_upcoming_event_submissions = CalendarSubmission.objects.filter(published=True).filter(Q(opening__gte=today())).order_by('opening')
     return render(request, 'highlights.html', {'highlighted_submissions': highlighted_submissions, 'highlighted_event_submissions': highlighted_event_submissions, 'all_current_event_submissions': all_current_event_submissions, 'all_upcoming_event_submissions': all_upcoming_event_submissions, 'today': today(), 'today_plus_2w': today_plus_2w()})
