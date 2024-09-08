59a7febf09c4ee38868ed2945c1cff9323845712
Anders Aarvik <aarvik92@gmail.com>
2024-09-08 12:52:47 +0200

    Release/v1.1.10 (#48)
    * Added attachment option to pinboard
    
    * Changes to mobile navigation
    
    ---------
    
    Co-authored-by: Sofie Meta <43446790+atmeta@users.noreply.github.com>

--- ps_pinboard/admin.py
+++ ps_pinboard/admin.py
@@ -1,14 +1,19 @@
 # ps_calendar/admin.py
 from django.contrib import admin
-from .models import PinBoard
-from .forms import PinboardAdminForm
+from .models import Pinboard, PinboardImages
+from .forms import PinboardAdminForm, PinboardImagesForm
 
+class PinboardImagesInline(admin.TabularInline):  # or admin.StackedInline for a different layout
+    model = PinboardImages
+    form = PinboardImagesForm
+    extra = 1  # Number of empty forms to display
 
-class PinBoardAdmin(admin.ModelAdmin):
+class PinboardAdmin(admin.ModelAdmin):
     form = PinboardAdminForm
     list_display = ["title", "category", "created_at", "published"]
+    inlines = [PinboardImagesInline]
     # list_filter = ["created_at", "expiration_date"]
     search_fields = ["title"]
 
 
-admin.site.register(PinBoard, PinBoardAdmin)
+admin.site.register(Pinboard, PinboardAdmin)
